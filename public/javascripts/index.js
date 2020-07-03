7411b0a104498de8134c6c3e3211e817f67eee84
Ekta Oza <ektaoza@Ektas-MacBook-Pro.local>
2020-07-02 21:20:55 -0700

    Add more changes

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,19 +1,199 @@
-$(document).ready(function(){
-    $( ".newGame" ).click(function() {
-        $(".welcomeDiv" ).hide();
-        $(".enterGameDiv").removeClass("d-none");
-    });
-
-    $(".enterGameButton").click(function() {
-        var name = $("#name").val();
-        Cookies.set('player_name', name);
-    });
-});
-
-var onPageLoad = function() {
-    var name = Cookies.get('player_name');
-    if (name != null) {
-        //Get state
-        $(".welcomeDiv" ).hide();
+window.onload = function () {
+    const WORD_URL = "/listWords";
+    const GAME_URL = "/games";
+    var shuffleArray = function( array ) {
+        var counter = array.length, temp, index;
+        // While there are elements in the array
+        while ( counter > 0 ) {
+            // Pick a random index
+            index = Math.floor( Math.random() * counter );
+    
+            // Decrease counter by 1
+            counter--;
+    
+            // And swap the last element with it
+            temp = array[ counter ];
+            array[ counter ] = array[ index ];
+            array[ index ] = temp;
+        }
+        return array;
     }
-}
+    var postRequestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" }
+      };
+    var app = new Vue({
+        el: '#app',
+        data: {
+            isWelcomeHidden: true,
+            isStartGameHidden: true,
+            isJoinGameHidden: true,
+            isGameInProgress: false,
+            playerName: '',
+            gameId: null,
+            gameDescription: '',
+            showWords: false,
+            wordsPassed: [],
+            wordsGuessed: [],
+            wordsLeft: [],
+            currentWord: '',
+            showPass: true,
+            score: { 'A': 0, 'B': 0},
+            round: 1,
+            currentTeam: 'A',
+            showScores: false
+        },
+        mounted: function(){
+            this.refreshPage()
+        },
+        methods: {
+            startNew: function (event) {
+                this.isWelcomeHidden = true;
+                this.isStartGameHidden = false;
+            },
+            joinGame: function(event) {
+                this.isWelcomeHidden = true;
+                this.isJoinGameHidden = false;
+            },
+            enterGame: function(event) {
+                Cookies.set('player_name', this.playerName);
+                this.isWelcomeHidden = true;
+                this.isStartGameHidden = true;
+                this.isGameInProgress = true;
+                fetch(WORD_URL)
+                    .then(response => response.json())
+                    .then(result => {
+                        this.wordsLeft = result;
+                        console.log(this.wordsLeft);
+                        postRequestOptions.body = JSON.stringify({
+                            words_left: result,
+                            words_guessed: [],
+                            words_passed: [],
+                            round: 1,
+                            score: this.score,
+                            currentTeam: this.currentTeam
+                        })
+                        fetch(GAME_URL, postRequestOptions)
+                            .then(response => response.json())
+                            .then(data => {
+                                this.gameId = data.id;
+                                this.gameDescription = 'Game '+this.gameId+' in progress. Please let others know';
+                                Cookies.set('game_id', this.gameId);
+                            });
+                    });
+            },
+            enterGameGuest: function(event) {
+                Cookies.set('player_name', this.playerName);
+                Cookies.set('game_id', this.gameId);
+                this.isWelcomeHidden = true;
+                this.isStartGameHidden = true;
+                this.isGameInProgress = true;
+                this.isJoinGameHidden = true;
+                fetch(GAME_URL+'/'+this.gameId)
+                    .then(response => response.json())
+                    .then(result => {
+                        console.log(result);
+                        this.gameDescription = 'Game '+this.gameId+' in progress. Please wait for your turn';
+                        this.wordsLeft = result.words_left;
+                        this.score = result.score;
+                        this.currentTeam = result.currentTeam;
+                    });
+            },
+            refreshPage: function(event) {
+                var playerName = Cookies.get('player_name');
+                if(playerName != null) {
+                    this.gameId = Cookies.get('game_id');
+                    this.gameDescription = 'Game '+this.gameId+' in progress. Please wait for your turn';
+                    this.isWelcomeHidden = true;
+                    this.isGameInProgress = true;
+                } else {
+                    this.isWelcomeHidden = false;
+                    this.isGameInProgress = false;
+                }
+            },
+            clearCookies: function(event) {
+                Cookies.remove('player_name');
+                Cookies.remove('game_id');
+                this.refreshPage();
+            },
+            beginGame: function(event) {
+                fetch(GAME_URL+'/'+this.gameId)
+                    .then(response => response.json())
+                    .then(result => {
+                        console.log(result);
+                        this.gameDescription = 'Game '+this.gameId+' in progress. Please wait for your turn';
+                        this.wordsLeft = result.words_left;
+                        this.currentWord = this.wordsLeft.pop();
+                        this.score = result.score;
+                        this.currentTeam = result.currentTeam;
+                    });
+                this.isGameInProgress = false;
+                this.showWords = true;
+            },
+            resumeGame: function(event) {
+                this.wordsLeft = this.wordsLeft.concat(this.wordsPassed);
+                if (this.wordsLeft.length == 0) {
+                    alert("End of round");
+                    this.round++;
+                    return;
+                }
+                this.showWords = true;
+                this.wordsLeft = shuffleArray(this.wordsLeft);
+                this.currentWord = this.wordsLeft.pop();
+            },
+            pass: function(event) {
+                this.wordsPassed.push(this.currentWord);
+                if (this.wordsLeft.length == 0) {
+                    alert("End of round");
+                    this.round++;
+                    this.toggleTeam();
+                    return;
+                }
+                this.currentWord = this.wordsLeft.pop();
+                this.showPass = false;
+            },
+            correct: function(event) {
+                this.wordsGuessed.push(this.currentWord);
+                if (this.wordsLeft.length == 0) {
+                    alert("End of round");
+                    this.round++;
+                    return;
+                }
+                this.currentWord = this.wordsLeft.pop();
+                this.score[this.currentTeam]++;
+            },
+            endTurn: function(event) {
+                this.toggleTeam();
+                this.resumeGame();
+                this.showScores = true;
+                this.showWords = false;
+                console.log(this.wordsLeft);
+                console.log(this.wordsPassed);
+                this.wordsLeft = this.wordsLeft.concat(this.wordsPassed);
+                this.wordsLeft = shuffleArray(this.wordsLeft);
+                postRequestOptions.body = JSON.stringify({
+                    words_left: this.wordsLeft,
+                    words_guessed: [],
+                    words_passed: [],
+                    round: 1,
+                    id: this.gameId,
+                    score: this.score,
+                    currentTeam: this.currentTeam
+                })
+                fetch(GAME_URL, postRequestOptions)
+                    .then(response => response.json())
+                    .then(data => {
+                        this.gameId = data.id;
+                        this.gameDescription = 'Game '+this.gameId+' in progress. Please wait for your turn';
+                    });
+            },
+            toggleTeam: function(event) {
+                if (this.currentTeam === 'A') {
+                    this.currentTeam = 'B';
+                } else {
+                    this.currentTeam = 'A';
+                }
+            }
+          }
+    })
+}   
--- routes/game.js
+++ routes/game.js
@@ -1,7 +1,9 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://kshetty:wvwCuUFBOuyguqxGckfbFzl0pYr5KF1PCwfEABefzGFzTaUNUPEpkK7udGPFJ7I2n8NMuHW7i3wNMJzZDKOHMA==@kshetty.mongo.cosmos.azure.com:10255/?ssl=true&appName=@kshetty@';
 var findWords = function(db, resultWords, callback) {
-    var cursor =db.collection('words').find({"type": "A"});
+    var randomNum = Math.floor(Math.random() * 3);
+    var type = String.fromCharCode(65+randomNum);
+    var cursor =db.collection('words').find({"type": type});
     cursor.each(function(err, doc) {
         if (doc != null) {
             console.dir(doc);
@@ -22,6 +24,54 @@ module.exports = function(app) {  //receiving "app" instance
                 res.send(resultWords);
                 client.close();
             });
-        })
+        });
+    })
+
+    app.get('/games/:id', function (req, res) {
+        console.log("Here");
+        const gameId = Number(req.params.id);
+        MongoClient.connect(url, function(err, client) {
+            var db = client.db('gamesDB');
+            var games = db.collection('games').find({"id": gameId});
+            games.each(function(err, doc) {
+                if (doc != null) {
+                    res.send(doc);
+                }
+            });
+            client.close();
+        });
+    })
+
+    app.post('/games', function (req, res) {
+        const body = req.body;
+        console.log(body);
+        MongoClient.connect(url, function(err, client) {
+            if('id' in body) {
+                var gameId = Number(body.id);
+            } else {
+                var gameId = Math.floor((Math.random() * 1000) + 1);
+            }
+            var db = client.db('gamesDB');
+            db.collection('games').updateOne( {"id": gameId}, { $set: {
+                    "id": gameId,
+                    "words_guessed": body.words_guessed,
+                    "words_passed": body.words_passed,
+                    "words_left": body.words_left,
+                    "round": body.round,
+                    "score": body.score,
+                    "currentTeam": body.currentTeam
+                }},
+                {
+                    upsert: true
+                },
+                function(err, result) {
+                    console.log(result);
+                    console.log(err);
+                    console.log("Inserted a document into the games collection.");
+                    client.close();
+                    res.send({id: gameId});
+                }
+            );
+        });
     })
 }
